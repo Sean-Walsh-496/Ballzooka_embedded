dc17d3002e2b79bc910a209d790d312fc27f4d4c
sean <seantheprofessional1@gmail.com>
2026-02-04 14:04:19 -0600

    very basics working with state machine

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -1,9 +1,78 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino_RouterBridge.h> // Micro-controller cannot access USB-C serial
 
+// PROGRAM SETTINGS
+#define CONSOLE_LOGGING true
+
+// ENUMS =======================================================================
+enum State {
+  CONNECT,
+  IDLE_SAFE,
+  IDLE_DANGER,
+  REPOSITION,
+  STANDBY,
+  LAUNCH
+};
+
+
+// GLOBALS =====================================================================
+State currentState;
+const char* stateNames[] = {"CONNECT", "IDLE_SAFE", "IDLE_DANGER", "REPOSITION", "STANDBY", "LAUNCH"};
+
+
+// FUNCTIONS ===================================================================
+void PrintStatus() {
+  Monitor.print("Current state is: ");
+  Monitor.println(stateNames[currentState]);
+}
+
+bool HasBluetoothConnection() {
+  // TODO: finish this function!
+  return true;
+}
+
+bool HasValidTarget() {
+  return false;
+}
+
+State HandleConnect() {
+  if (HasBluetoothConnection()) {
+    return IDLE_SAFE;
+  }
+  return CONNECT;
+}
+
+State HandleIdleSafe() {
+  if (HasValidTarget()) {
+    return REPOSITION;
+  }
+  return IDLE_SAFE;
+}
+
+
+void setup() {
+  currentState = CONNECT;
+
+  Monitor.begin();
+  delay(1000);
+  Monitor.println("Ballzooka powered on.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // check for failures
+
+  switch(currentState) {
+    case CONNECT:
+      currentState = HandleConnect();
+      break;
+    case IDLE_SAFE:
+      currentState = HandleIdleSafe();
+      break;
+  }
+
+  // log and change LED
+  if (CONSOLE_LOGGING) {
+    PrintStatus();
+  }
+
 
 }
